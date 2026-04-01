34120e8a76eab8d5ea25b3101d6247d8ef955ca4
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-04-01 09:18:49 +0300

    add solution for merging 2 sorted linked lists

---
+++ merge_ists.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1 or not list2:
+            return list1 or list2
+        
+        prev = dummy_node = ListNode()
+        while list1 and list2:
+            if list1.val <= list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+                
+            prev = prev.next
+            
+        while list1:
+            prev.next = list1
+            list1 = list1.next
+            prev = prev.next
+            
+        while list2:
+            prev.next = list2
+            list2 = list2.next
+            prev = prev.next
+            
+        return dummy_node.next
