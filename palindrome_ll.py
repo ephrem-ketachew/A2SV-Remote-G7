a4c2b74962494f04f24574ee49fd1fdb1a3a39b1
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-04-02 17:52:38 +0300

    add solution for palindrome linked list

---
+++ palindrome_ll.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # arr = []
+        # while head:
+        #     arr.append(head.val)
+        #     head = head.next
+            
+        # left, right = 0, len(arr) - 1
+        # while left < right:
+        #     if arr[left] != arr[right]:
+        #         return False
+        #     left += 1
+        #     right -= 1
+            
+        # return True
+        
+        if not head or not head.next:
+            return True
+        
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+        prev = None
+        cur = slow
+        while cur:
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
+            
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+            
+        return True
