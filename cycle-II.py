61107ebc4c36df3882c5f7e70acdcb925f64b8b4
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-04-02 16:19:22 +0300

    add solution for cycle detection

---
+++ cycle-II.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None
+
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                break
+            
+        if slow != fast:
+            return None
+        
+        fast = head
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+            
+        return slow
