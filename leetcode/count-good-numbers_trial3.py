aee188ccb27be36ba119d305282b1115fde34c4e
Ephrem Ketachew <ephremketachew@gmail.com>
2026-04-15 16:59:01 +0300

    Add solution for Count Good Numbers (trial 3)

---
+++ leetcode/count-good-numbers_trial3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        MOD = 10 ** 9 + 7
+        even_count = (n + 1) // 2
+        odd_count = n // 2
+        
+        ans = pow(5, even_count, MOD) * pow(4, odd_count, MOD)
+        ans %= MOD
+        
+        return ans
