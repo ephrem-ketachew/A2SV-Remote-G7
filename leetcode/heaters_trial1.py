5b5e4691e55a57d16dc513536cfeda91c4e08ba8
Ephrem Ketachew <ephremketachew@gmail.com>
2026-04-23 23:06:12 +0300

    Add solution for Heaters (trial 1)

--- leetcode/heaters_trial1.py
+++ leetcode/heaters_trial1.py
@@ -2,16 +2,14 @@ class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         houses.sort()
         heaters.sort()
-        
-        n, m = len(houses), len(heaters)
-        i = j = 0
+
         min_radius = float('-inf')
-        for i in range(n):
-            radius = abs(heaters[j] - houses[i])
-            while j + 1 < m and radius >= abs(heaters[j + 1] - houses[i]):
-                radius = abs(heaters[j + 1] - houses[i])
-                j += 1
+        for house in houses:
+            idx = bisect_right(heaters, house)
+            pos1 = heaters[idx - 1] if idx - 1 >= 0 else float('inf')
+            pos2 = heaters[idx] if idx < len(heaters) else float('inf')
             
+            radius = min(abs(house - pos1), abs(house - pos2))
             min_radius = max(min_radius, radius)
             
         return min_radius
